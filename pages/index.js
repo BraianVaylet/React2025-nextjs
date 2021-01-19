b728729aace926762c48968525d5f6d235de04b4
braian vaylet <braianvaylet@eycon.com>
2021-01-19 18:33:32 -0300

    Hellow, world!

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Fast <a href="https://nextjs.org">Feedback</a>
         </h1>
 
         <p className={styles.description}>
